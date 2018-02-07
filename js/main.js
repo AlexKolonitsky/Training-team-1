92ed5da02d1d53151d53cd955d06cdc26117feb4
stasyushkevich <stas.aaron13@yandex.ru>
2018-02-07 23:39:42 +0300

    First version

---
+++ js/main.js
@@ -0,0 +1,14 @@
+but.onclick = function () {
+
+    var text1 = document.getElementsByTagName("input")[0];
+    var val1 = text1.value;
+    var text2 = document.getElementsByTagName("input")[1];
+    var val2 = text2.value;
+    var text3 = document.getElementsByTagName("input")[2];
+    var val3 = text3.value;
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val1) != true || val2 == '' || val3 == '') {
+        alert('Проверьте введенные данные');
+    } else {
+        alert('Все ок');
+    }
+};
