14cc4255ec425446db97e017bab48f139e3b2c54
stasyushkevich <stas.aaron13@yandex.ru>
2018-02-08 21:55:43 +0300

    update validation system

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ but.onclick = function () {
     var val2 = text2.value;
     var text3 = document.getElementsByTagName("input")[2];
     var val3 = text3.value;
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val1) != true || val2 == '' || val3 == '') {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val1) != true || val2 == false || val3 == false ) {
         alert('Проверьте введенные данные');
     } else {
         alert('Все ок');
