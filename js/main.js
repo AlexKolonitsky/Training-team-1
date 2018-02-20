5f69674ad8e57dc54abe314715f0852d6a5e89dc
Куц Егор Николаевич <Kutz@NLA.LOCAL>
2018-02-20 09:32:45 +0300

    1 Fix JS and CSS

--- js/main.js
+++ js/main.js
@@ -1,28 +1,32 @@
 window.onload = function () {
 
+  var inputs = document.getElementsByTagName('input');
+  var phonePattern = /[0-9\-\(\)\s]{9}/;
+  var signUpButton = document.getElementById('signUp');
+
   document.getElementById('confirm').onclick = function () {
-    var inputs = document.getElementsByTagName('input');
     for (var index = 0; index < inputs.length; ++index) {
       if (!inputs[index].value) {
         inputs[index].classList.add('wrongValue');
       }
     }
 
-    var pattern = /[0-9\-\(\)\s]{9}/;
+    
+    
     var phoneInput = document.getElementById('phoneNumber');
-    if (!pattern.test(phoneInput.value)) {
-      phoneInput.value = "";
+    if (!phonePattern.test(phoneInput.value)) {
+      phoneInput.value = '';
       phoneInput.classList.add('wrongValue');
     }
   };
 
-  if(document.getElementById('signUp')){
-    document.getElementById('signUp').onclick = function () {
-      window.location.replace("register.html");
+  if(signUpButton){
+    signUpButton.onclick = function () {
+      window.location.replace('register.html');
     };
   }
 
-  var inputs = document.getElementsByTagName('input');
+
   for (var index = 0; index < inputs.length; ++index) {
     inputs[index].onfocus = function () {
       this.classList.remove('wrongValue');
@@ -40,6 +44,6 @@ function previewFile() {
   if (file) {
     reader.readAsDataURL(file);
   } else {
-    preview.src = "";
+    preview.src = '';
   }
 }
