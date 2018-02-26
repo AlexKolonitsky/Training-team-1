9278e3455bfa16ac863b7cfa9e7845d0033ba935
MaxioN03 <maxion03@gmail.com>
2018-02-26 10:58:28 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+
+    $('.confirm').click(function(){
+
+        $('input').each(function(){
+            if(!$(this).val()){
+                //alert('Fill field '+$(this).attr('placeholder'));
+                $(this).addClass('wrongValue');
+            }
+        })
+    });
+
+    $('input').focus(function(){
+        $(this).removeClass('wrongValue');
+    })
+
+})
+
+
